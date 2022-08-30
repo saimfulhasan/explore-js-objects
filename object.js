6fe4deb66e22c8f8517a6e8210a45bcaabc2f635
saimfulhasan <saifulsaimshamim@gmail.com>
2022-08-30 23:29:27 +0600

    ways to creare an object

---
+++ object.js
@@ -0,0 +1,53 @@
+// Create object using object literals
+const player = {};
+player.name = 'Small Nirob'; /* Object property */
+player.speciality = 'Batsman'; /* Object property */
+// console.log(player);
+
+// Object Method
+player.bat = function () {
+    console.log('Swing your bat')
+} 
+console.log(player);
+player.bat();
+
+
+// We will use the following method most frequently
+const student = {
+    name: 'Pandey',
+    job: 'khai andey',
+    ball: function(){
+        console.log('throw the ball')
+    },
+    salary: 100000
+}
+
+
+
+// 2. object constructor
+const person = new Object();
+// console.log(person);
+
+// 3. object create method
+// const item = Object.create(null);
+const atel = Object.create(student);
+// console.log(atel.name); /* Output: Pandey */
+
+// 4. class
+class Person{
+    name = 'abul';
+    address = 'sodor ghat';
+    constructor(age){
+        this.age = age;
+    }
+}
+const person1 = new Person(56);
+console.log(person1);
+
+
+// 5. Function
+function Car(model, price){
+    this.model = model;
+    this.price = price;
+}
+const tesla = new Car('elon', 32);
